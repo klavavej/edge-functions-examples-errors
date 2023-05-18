f37e5c4a997f2511fe21bafbea4d5d71e7003437
Kristen Lavavej <35638702+klavavej@users.noreply.github.com>
2023-05-18 10:31:29 -0700

    Update and rename abtest.ts to abtest.js

--- netlify/edge-functions/abtest.js
+++ netlify/edge-functions/abtest.js
@@ -1,6 +1,6 @@
 import { Config } from "https://edge.netlify.com"
 
-export default async () => throw new Error("💥")
+export default async () => throw new Error("oops")
 
 export const config: Config = {
   path: "/hello"
